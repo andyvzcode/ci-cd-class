33a3579e865f9c60b5f4e9d8056a3b3d2c3334ac
andyvzcode <andresvz91@gmail.com>
2025-11-13 19:22:13 -0500

    validacion action new

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ def main():
     print("Hello from python-app-ci!")
 
 
+def sum_two_numbers(a: int, b: int) -> int:
+    return a + b
+
+
 if __name__ == "__main__":
     main()
--- tests.py
+++ tests.py
@@ -1,2 +1,9 @@
-def test_main():
-    assert 1 == 0
+from main import sum_two_numbers
+
+
+def test_sum_two_numbers():
+    assert sum_two_numbers(1, 2) == 3
+    assert isinstance(sum_two_numbers(1, 2), int)
+
+    assert sum_two_numbers(1, 2) > 2
+    assert not isinstance(sum_two_numbers(1, 2), float)
